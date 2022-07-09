da5b9d44368e3c00447eec363d232f6a0bae7bdd
WangZemin0816 <30365426+WangZemin0816@users.noreply.github.com>
2022-07-09 17:36:04 +0800

    fix

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,12 +3,21 @@ use std::fs::File;
 
 use std::net::TcpStream;
 use std::thread::JoinHandle;
+use crate::client::adb_client::AdbClientImpl;
 
 use crate::error::adb::AdbError;
 
 mod adb_client;
 mod device_client;
 mod tests;
+
+fn new_adb_client(host: &String, port: i32, bin_path: String) -> Box<dyn AdbClient> {
+    Box::new(AdbClientImpl {
+        host: host.clone(),
+        port: port.clone(),
+        bin_path: bin_path.clone(),
+    })
+}
 
 pub trait AdbClient {
     fn start_server(&mut self) -> Result<(), AdbError>;
